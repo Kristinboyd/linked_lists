0626e9339f83c081a24d2de19fb6818d707b9c0d
Kristin <Kristineliseboyd13@gmail.com>
2024-04-22 09:26:41 -0700

    completes pop_first'

--- LL-Pop_First.py
+++ LL-Pop_First.py
@@ -55,13 +55,17 @@ class LinkedList:
         self.length += 1
         return True
 
-    ## WRITE POP_FIRST METHOD HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
-
+    # pops off first node and returns it
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head 
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1 
+        if self.length == 0:
+            self.tail = None
+        return temp
 
 
 
